721f3eed23895745f52d0e2bf6b840f309d5e4f3
Adnan <adnan.chowdhury@outlook.com>
2020-03-01 22:03:23 -0600

    named function

--- intro/functions.exs
+++ intro/functions.exs
@@ -1,7 +1,13 @@
-fn
-  # testing some function
-  a, b -> a + b
+defmodule Greeter do
+  def for(name, greeting) do
+    fn
+      ^name -> "#{greeting} #{name}"
+      _ -> "I don't know you"
+    end
+  end
 end
 
-swap = fn {a, b} -> {b, a} end
-swap.({6, 8})
+mr_valim = Greeter.for("Jose", "Oi")
+
+IO.puts(mr_valim.("Jose"))
+IO.puts(mr_valim.("Dave"))
