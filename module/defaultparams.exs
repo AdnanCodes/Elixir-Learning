45e40a2a16d3222b4cf7aa62f1baf2ba4ac99036
Adnan <adnan.chowdhury@outlook.com>
2020-03-11 22:30:14 -0500

    Multiple functions defaults

--- module/defaultparams.exs
+++ module/defaultparams.exs
@@ -10,3 +10,24 @@ Example.func("a", "b")
 Example.func("a", "b", "c")
 # => ["a","b","c","d"]
 Example.func("a", "b", "c", "d")
+
+defmodule Params do
+  def func(p1, p2 \\ 123)
+
+  def func(p1, p2) when is_list(p1) do
+    "You said #{p2} with a list"
+  end
+
+  def func(p1, p2) do
+    "You passed in #{p1} and #{p2}"
+  end
+end
+
+# You passed in 99 and 123
+IO.puts(Params.func(99))
+# You passed in 99 and cat
+IO.puts(Params.func(99, "cat"))
+# You said 123 with a list
+IO.puts(Params.func([99]))
+# You said dog with a list
+IO.puts(Params.func([99], "dog"))
