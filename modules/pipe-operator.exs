790779a60ac8eaa8d96230e1b72df595b0092750
Adnan <adnan.chowdhury@outlook.com>
2020-03-15 14:18:10 -0500

    fix notes

--- modules/pipe-operator.exs
+++ modules/pipe-operator.exs
@@ -2,6 +2,6 @@
 # ["Elixir", "rocks"]
 "Elixir rocks" |> String.upcase() |> String.split()
 # ["ELIXIR", "ROCKS"]
-# Line 3 could have be written otherwise as String.upcase(String.Split("Elixir Rocks")) in other langunages
+# Line 3 could have be written otherwise as String.split(String.upcase("Elixir Rocks")) in other langunages
 # It is somewhat the reverse of stacking function calls, but more intuitive, Apply function on some something, chain it further to get your result
 # Versus wrapping around the data
