0a4a706d100afbf7778b03c516792aff41083dc3
Adnan <adnan.chowdhury@outlook.com>
2020-03-25 20:58:56 -0500

    additional functions and map function

--- lists/list.exs
+++ lists/list.exs
@@ -1,4 +1,4 @@
-defmodule Mylist do
+defmodule MyList do
   # if empty list do not add length
   def len([]), do: 0
 
@@ -6,4 +6,16 @@ defmodule Mylist do
   def len([_head | tail]), do: 1 + len(tail)
 
   # underscore acts placeholder and you can use to make it easier to detect unused variables or parameters
+
+  # recursive squaring calls
+  def square([]), do: []
+  def square([head | tail]), do: [head * head | square(tail)]
+
+  def add_1([]), do: []
+  def add_1([head | tail]), do: [head + 1 | add_1(tail)]
+
+  # map function similar to JavaScript map method on arrays accepts callback
+
+  def map([], _func), do: []
+  def map([head | tail], func), do: [func.(head) | map(tail, func)]
 end
